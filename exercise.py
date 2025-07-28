ade81fe5cb2600ce86e9e7d9ebba64e05b32c364
esdmr <esdmr0@gmail.com>
2025-07-28 23:45:36 +0330

    Add files

--- exercise.py
+++ exercise.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from forms import LoginForm
 
 app = Flask(__name__)
 
+app.config.from_object('default_config')
+app.config.from_prefixed_env()
+
 @app.route('/')
-def index():
-    return 'Hello, World!'
+def login_ep():
+    form = LoginForm()
+
+    return render_template('login.html', form=form)
